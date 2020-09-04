c538c3f6279687cc6964f395019f464bb9825cee
Adam Lopez <adamf04@gmail.com>
2020-09-04 17:22:13 -0400

    adding rest of CRUD to my one to many user/posts relationship using laravel

--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,51 @@ Route::get('/create', function() {
 
     $user = User::findOrFail(1);
 
-    $post = new Post(['title'=>'This is my cool title','body'=>'This is some cool body text using laravel']);
+    $post = new Post(['title'=>'This is my second title','body'=>'This is some cool body text using laravel']);
 
     $user->posts()->save($post);
+
+});
+
+/* 
+* |--------------------------------------------------------------------------
+* | READ
+* |-------------------------------------------------------------------------- 
+*/
+Route::get('/read',function() {
+    
+    $user = User::findOrFail(1);
+
+    foreach ($user->posts as $posts ) {
+        echo $posts->title . "<br>";
+    }
+
+});
+
+/* 
+* |--------------------------------------------------------------------------
+* | UPDATE
+* |-------------------------------------------------------------------------- 
+*/
+Route::get('/update', function() {
+    $user = User::find(1);
+
+    // two ways to update using whereId (camelcase) & where()
+    // $user->posts()->whereId(1)->update(['title'=>'I have updated the title. I love this shit.']);
+    
+    $user->posts()->where('id','=', 2)->update(['title'=>'I have updated the title. I love this shit.']);
+
+});
+
+/* 
+* |--------------------------------------------------------------------------
+* | DELETE
+* |-------------------------------------------------------------------------- 
+*/
+Route::get('/delete',function() {
+
+    $user = User::find(1);
+
+    $user->posts()->whereId(1)->delete();
 
 });
